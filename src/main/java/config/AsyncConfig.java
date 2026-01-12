86609a34f4c24e7f2f9c0ec593e32c49c8991ba5
Sai Kiran Kondapalli <saikirankondapalli03@gmail.com>
2026-01-12 00:25:47 -0500

    mavenized it

--- src/main/java/config/AsyncConfig.java
+++ src/main/java/config/AsyncConfig.java
@@ -1,3 +1,11 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 @Configuration
 @EnableAsync
 public class AsyncConfig {
